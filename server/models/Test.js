5d091430825df4fbd4c35f97502a52be4d43329d
Shai Moran <shaimoran13@gmail.com>
2022-02-14 17:48:37 +0200

    Added test model and updated user and question models

---
+++ server/models/Test.js
@@ -0,0 +1,46 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const TestSchema = {
+  Name: {
+    type: String,
+    required: true
+  },
+  language: {
+    type: String,
+    enum: ['English, Hebrew'],
+    required: true
+  },
+  opening: {
+    type: String,
+    required: true
+  },
+  questions: {
+    type: Array,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  answerReview: {
+    type: Boolean,
+    required: true
+  },
+  certificateUtl: {
+    type: String,
+    required: true
+  },
+  passingText: {
+    type: String,
+    required: true
+  },
+  failText: {
+    type: String,
+    required: true
+  }
+};
+
+const Test = mongoose.model('Test', TestSchema);
+
+module.exports = Test;
