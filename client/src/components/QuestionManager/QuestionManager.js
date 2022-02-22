2a2b0e07d1743dffec24223b6b039864ed7444f0
Maxim Shvartzbaum <maxim8844@gmail.com>
2022-02-22 14:25:14 +0200

    has changed question manager

--- client/src/components/QuestionManager/QuestionManager.js
+++ client/src/components/QuestionManager/QuestionManager.js
@@ -17,10 +17,13 @@ const QuestionManager = () => {
     <Table celled selectable>
       <Table.Header>
         <Table.Row>
+          <Table.HeaderCell>Question Type</Table.HeaderCell>
           <Table.HeaderCell>Title</Table.HeaderCell>
-          <Table.HeaderCell>Number Of Answers</Table.HeaderCell>
-          <Table.HeaderCell>Last Updated</Table.HeaderCell>
-          <Table.HeaderCell></Table.HeaderCell>
+          <Table.HeaderCell>Content</Table.HeaderCell>
+          <Table.HeaderCell>Answers</Table.HeaderCell>
+          <Table.HeaderCell>View Answers</Table.HeaderCell>
+          <Table.HeaderCell>Tags</Table.HeaderCell>
+          <Table.HeaderCell>Points</Table.HeaderCell>
         </Table.Row>
       </Table.Header>
       <Table.Body>
