2c3fc527c6b897a395f4c33b678463eed0528908
Shai Moran <shaimoran13@gmail.com>
2022-02-23 16:17:28 +0200

    test manager changes

--- client/src/components/TestManager/TestManager.js
+++ client/src/components/TestManager/TestManager.js
@@ -1,43 +1,59 @@
 import React, { useEffect, useState } from 'react';
-import { Container, Table } from 'semantic-ui-react';
+import { Container, Table, Input } from 'semantic-ui-react';
 import TestRow from './TestRow/TestRow';
 import allTestsService from '../../services/allTestsService';
 
 const TestManager = () => {
   const [tests, setTests] = useState([]);
+  const [search, setSearch] = useState('');
 
   useEffect(async () => {
-    const urlParams = new URLSearchParams(window.location.search);
-    const field = urlParams.get('field');
-    var testsData = await allTestsService.getAllTests(field);
-    testsData.data.map((test) => {
-      setTests((prevState) => [...prevState, test]);
-    });
-  }, []);
+    if (search === '') {
+      const urlParams = new URLSearchParams(window.location.search);
+      const field = urlParams.get('field');
+      var testsData = await allTestsService.getAllTests(field);
+      testsData.data.map((test) => {
+        setTests((prevState) => [...prevState, test]);
+      });
+    } else {
+        
+    }
+  }, [search]);
 
   return (
-    <Table celled selectable>
-      <Table.Header>
-        <Table.Row>
-          <Table.HeaderCell>Name</Table.HeaderCell>
-          <Table.HeaderCell>Number Of Questions</Table.HeaderCell>
-          <Table.HeaderCell>Last Updated</Table.HeaderCell>
-          <Table.HeaderCell></Table.HeaderCell>
-        </Table.Row>
-      </Table.Header>
-      <Table.Body>
-        {tests.map((test) => {
-          return (
-            <TestRow
-              id={test.id}
-              name={test.name}
-              numOfQuestions={test.questions.length}
-              lastUpdated={test.lastUpdated}
-            />
-          );
-        })}
-      </Table.Body>
-    </Table>
+    <div>
+      <Header textAlign="left" as="h1">
+        Test Managet:
+      </Header>
+      <Input
+        onChange={(e) => setSearch(e.target.value)}
+        type="text"
+        label="Search for test by name:"
+      />
+      <Table celled selectable>
+        <Table.Header>
+          <Table.Row>
+            <Table.HeaderCell>Name</Table.HeaderCell>
+            <Table.HeaderCell>Number Of Questions</Table.HeaderCell>
+            <Table.HeaderCell>Last Updated</Table.HeaderCell>
+            <Table.HeaderCell></Table.HeaderCell>
+          </Table.Row>
+        </Table.Header>
+        <Table.Body>
+          {tests.map((test) => {
+            return (
+              <TestRow
+                id={test.id}
+                testUrl={test.testUrl}
+                name={test.name}
+                numOfQuestions={test.questions.length}
+                lastUpdated={test.lastUpdated}
+              />
+            );
+          })}
+        </Table.Body>
+      </Table>
+    </div>
   );
 };
 
