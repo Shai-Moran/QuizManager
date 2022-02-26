aa7753a920a21b4411a9e7100e93ab4708883774
Shai Moran <shaimoran13@gmail.com>
2022-02-26 23:40:03 +0200

    Added a test instance module

---
+++ server/models/TestInstance.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const TestInstanceSchema = {
+  id: {
+    type: String,
+    required: true
+  },
+  testId: {
+    type: String,
+    required: true
+  },
+  studentId: {
+    type: String,
+    required: true
+  },
+  questions: {
+    type: Array,
+    required: true
+  },
+  grade: {
+    type: Number,
+    required: true
+  }
+};
+
+const TestInstance = mongoose.model('TestInstance', TestInstanceSchema);
+
+module.exports = TestInstance;
